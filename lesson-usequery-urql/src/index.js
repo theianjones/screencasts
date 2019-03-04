6484aa86219b1be2c6c54cc5e44513f9b327ec96
Ian Jones <ian@egghead.io>
2019-03-04 10:13:14 -0500

    use final

--- lesson-usequery-urql/src/index.js
+++ lesson-usequery-urql/src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.start";
-import { Provider, createClient } from "urql";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.final'
+import {Provider, createClient} from 'urql'
 
 const client = createClient({
-  url: "https://egghead.io/graphql"
-});
+  url: 'https://egghead.io/graphql',
+})
 
 ReactDOM.render(
   <Provider value={client}>
     <App />
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
