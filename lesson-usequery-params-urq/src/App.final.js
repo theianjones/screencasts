2521c0b9226a7362f80ce0f0e20e5be8d47a6997
Ian Jones <ian@egghead.io>
2019-03-05 20:42:18 -0500

    finish lesson-usequery-params-urql

--- lesson-usequery-params-urq/src/App.final.js
+++ lesson-usequery-params-urq/src/App.final.js
@@ -1,28 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useQuery} from 'urql'
 
 const courseQuery = `
-  query courses {
-    courses {
+  query courses($page: Int) {
+    courses(page: $page) {
       title
     }
   }
 `
 
 function App() {
+  const [page, setPage] = useState(1)
   const [result] = useQuery({
     query: courseQuery,
+    variables: {
+      page,
+    },
   })
 
-  if (result.fetching) {
-    return 'Loading...'
-  } else if (result.error) {
+  if (result.error) {
     return 'There was an error :('
   }
 
   return (
     <div>
       <h1>egghead courses</h1>
+      <button onClick={() => setPage(page + 1)}>Next Page</button>
       {result && result.data && (
         <ul style={{listStyle: 'none'}}>
           {result.data.courses.map(({title}) => (
