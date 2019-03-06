a7d6de90e909afc358927ed93a7c09037a333184
Ian Jones <ian@egghead.io>
2019-03-05 20:31:42 -0500

    add start file

--- lesson-usequery-params-urq/src/App.final.js
+++ lesson-usequery-params-urq/src/App.final.js
@@ -1,28 +1,48 @@
-import React, {Component} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import {useQuery} from 'urql'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    )
+const courseQuery = `
+  query courses {
+    courses {
+      title
+    }
   }
+`
+
+function App() {
+  const [result] = useQuery({
+    query: courseQuery,
+  })
+
+  if (result.fetching) {
+    return 'Loading...'
+  } else if (result.error) {
+    return 'There was an error :('
+  }
+
+  return (
+    <div>
+      <h1>egghead courses</h1>
+      {result && result.data && (
+        <ul style={{listStyle: 'none'}}>
+          {result.data.courses.map(({title}) => (
+            <li
+              key={title}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                fontSize: 16,
+                fontFamily: 'sans-serif',
+                marginBottom: 10,
+              }}
+            >
+              {title}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
 }
 
 export default App
