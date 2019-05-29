cbe9f30dc777a5cdd5a6f1509438e7c751a10ed9
Ian Jones <ian@egghead.io>
2019-05-28 22:32:08 -0400

    fix index file

--- lesson-set-up-urql-provider/src/index.js
+++ lesson-set-up-urql-provider/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {createClient, Provider} from 'urql'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = createClient({
+  url: 'https://egghead.io/graphql',
+})
+
+ReactDOM.render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
